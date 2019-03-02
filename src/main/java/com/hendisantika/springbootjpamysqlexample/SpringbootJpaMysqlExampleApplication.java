e4af62a3b14b7db85994c548a3235ed90cd5e60f
hendisantika <hendisantika@yahoo.co.id>
2019-03-02 19:23:26 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/springbootjpamysqlexample/SpringbootJpaMysqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootjpamysqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaMysqlExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaMysqlExampleApplication.class, args);
+    }
+
+}
